8f7768a075dad404d6977fea6e20d7bb6716f80d
Tarabass <p.rietveld@live.com>
2019-02-20 22:32:54 +0100

    Initial

---
+++ compilesass.bat
@@ -0,0 +1,37 @@
+@echo off
+
+set theme=%1
+
+if not "%theme%" == "" (
+    GOTO THEME
+)
+
+GOTO DEFAULT
+
+:THEME
+    (
+        echo @import 'functions';
+        echo @import 'mixins';
+        echo @import 'themes/colors_%theme%';
+        echo @import 'themes/theme_%theme%';
+        echo @import 'themes/colors_default';
+        echo @import 'themes/theme_default';
+    ) > styles.scss
+
+    sass --watch styles.scss styles.css
+
+    GOTO END
+
+:DEFAULT
+    (
+        echo @import 'functions';
+        echo @import 'mixins';
+        echo @import 'themes/colors_default';
+        echo @import 'themes/theme_default';
+    ) > styles.scss
+
+    sass --watch styles.scss styles.css
+
+    GOTO END
+
+:END
